f626c751bb4b0f1d95d2fd2649c50e85d0d26151
Stuck <stuckf@dnb.com>
2021-10-27 08:22:59 -0400

    init

--- azurerm_storage_account.tf
+++ azurerm_storage_account.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "regions_logsa" {
   for_each = { for v in distinct(concat([var.region],var.regions)): v => v }
-    name                     = "${substr(var.name-vars["account"], 0, 24-(length(var.regions[count.index])+5))}${each.key}logsa"
+    name                     = "${substr(var.name-vars["account"], 0, 24-(length(each.key)+5))}${each.key}logsa"
     resource_group_name      = azurerm_resource_group.rg-acct.name
     location                 = each.key
     account_replication_type = var.account_replication_type

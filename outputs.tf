bad453b7505a423f282a58204c9d57558207ec51
Stuck <stuckf@dnb.com>
2021-10-27 08:11:42 -0400

    init

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "azurerm_storage_account_name" {
-  value = azurerm_storage_account.logsa.name
-}
-
 output "azurerm_storage_account_ids" {
   value = zipmap(var.regions, azurerm_storage_account.regions_logsa.*.id)
 }

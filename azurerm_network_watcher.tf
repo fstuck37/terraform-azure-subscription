57986094d876c5befa4acabef9aca7dd2a57e161
Stuck <stuckf@dnb.com>
2021-10-27 08:46:13 -0400

    init

--- azurerm_network_watcher.tf
+++ azurerm_network_watcher.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_watcher" "flow-log" {
   for_each = { for v in distinct(concat([var.region],var.regions)): v => v }
     name                = "${var.name-vars["account"]}-${each.key}-${var.name-vars["name"]}-watcher"
-    location            = azurerm_resource_group.rg-acct.location
+    location            = each.key
     resource_group_name = azurerm_resource_group.rg-acct.name
     tags                = var.tags
 }

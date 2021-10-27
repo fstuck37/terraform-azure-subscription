c41a71810226244f3756aa1fafffdb347aa64495
Stuck <stuckf@dnb.com>
2021-10-27 08:34:07 -0400

    init

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
+  
   azurerm_storage_account_ids = {
-    for k, stg in azurerm_storage_account.regions_logsa k => stg.id
+    for k, stg in azurerm_storage_account.regions_logsa : k => stg.id
   }
 }
